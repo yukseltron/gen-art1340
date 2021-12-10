97075236142e399993e30797f8359dfbf27ef70d
yukseltron <h.yuksel04@gmail.com>
2021-12-10 10:10:43 -0500

    added more dynamicism

--- newGUI.py
+++ newGUI.py
@@ -3,6 +3,11 @@ from PIL import Image
 import io
 
 
+class Enemy():
+    def __init__(self, path):
+        self.health = 4
+        self.path = path
+
 def healthBar(character, health):
     data = []
     for i in range(4):
@@ -15,21 +20,19 @@ def healthBar(character, health):
 def gameScreen():
     sg.theme('Black')
     armed = False
-    gif = "out.gif"
-    events = ["You get hit!", "You attack!", "You reload!", "You block!", "They get hit!", "They attack!", "They reload!", "They block!"]
-    enemy = Image.open(gif)
+    events = ("You get hit!", "You attack!", "You reload!", "You block!", "They get hit!", "They attack!", "They reload!", "They block!")
+    e = Enemy("out.gif")
+    enemy = Image.open(e.path)
     bioE = io.BytesIO()
-    event = events[0]
-    enemy.save(bioE, format="GIF")
-    col1 = [[sg.Image(key="-IMAGE-")], [sg.Button('New Enemy')]]
-    events = "You get hit"
+    enemy.save(bioE, format = "GIF")
+    col1 = [[sg.Image(key = "-IMAGE-")], [sg.Button('New Enemy')]]
     col2 = [[sg.Text('Enemy')],
     healthBar("EN",4),
-    [sg.Text(event)],
+    [sg.Text(events[0], key = '-event-')],
     [sg.Text('You')],
     healthBar("US",4),
-    [sg.Button("Reload", key = '-b-')],
-    [sg.Button('Block')]]
+    [sg.Button("Reload", key = '-action-')],
+    [sg.Button('Block', key = '-block-')]]
 
     layout = [[sg.Column(col1)], [sg.Column(col2)]]
     window = sg.Window('Window Title', layout, finalize = True)
@@ -38,16 +41,16 @@ def gameScreen():
         event, values = window.read(timeout=100)
         if event == sg.WIN_CLOSED:
             break
-        if event == '-b-':
+        if event == '-action-':
             if armed == False:
-                window['-b-'].Update("Attack")
+                window['-action-'].Update("Attack")
                 armed = True
             elif armed == True:
-                window['-b-'].Update("Reload")
+                window['-action-'].Update("Reload")
                 armed = False
         if event == "New Enemy":
             window["-IMAGE-"].update(data=bioE.getvalue())
-        window.Element('-IMAGE-').update_animation_no_buffering (gif)
+        window.Element('-IMAGE-').update_animation_no_buffering(e.path)
 
     window.close()
 
