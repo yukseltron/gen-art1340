bed73fd6cabb671704c19ba92a84eb25f6de1a02
yukseltron <h.yuksel04@gmail.com>
2021-12-12 16:33:51 -0500

    random enemies now generated

---
+++ Game.py
@@ -0,0 +1,222 @@
+import PySimpleGUI as sg
+import random
+from PIL import Image
+import io
+import GenerateArt as gen
+
+class Enemy():
+    def __init__(self, path):
+        self.health = 4
+        self.path = path
+        self.armed = None
+        self.block_tedency = 4
+        self.block = None;
+
+class Player():
+    def __init__(self):
+        self.health = 4
+        self.armed = None
+        self.block = None
+        self.score = 0
+
+class GameScreen():
+    def __init__(self):
+        self.player = Player()
+        gen.start_generator(2)
+        self.enemy = Enemy("out.gif")
+        sg.theme('Black')
+        self.col1 = [[sg.Image(key = "-IMAGE-")], [sg.Button('  ', disabled = True, key ="-nextenemy-")]]
+        self.col2 = [
+            [sg.Text("Health: " + str(self.enemy.health), key = '-enemyhealth-')],
+            self.healthBar("EN",self.enemy.health),#0
+            [sg.Text("Enemy Action", key = '-enemyevent-')],
+            [sg.Text("Welcome to the game!", key = '-outcome-')],
+            [sg.Text("Your Action", key = '-playerevent-')],
+            [sg.Text('Score: ' + str(self.player.score), key = '-score-')],
+            [sg.Text("Health: " + str(self.player.health), key = '-playerhealth-')],
+            self.healthBar("US",self.player.health),#5
+            [sg.Button("Reload", key = '-action-')],
+            [sg.Button('Block', key = '-block-')]
+        ]
+        self.layout = [[sg.Column(self.col1)], [sg.Column(self.col2)]]
+        self.window = sg.Window('Shot Art Game', self.layout, finalize = True)
+        self.loop()
+
+    def reset(self):
+        if self.player.health == 0:
+            self.player = Player()
+        else:
+            score = self.player.score
+            self.player = Player()
+            self.player.score = score
+
+        gen.start_generator(2)
+        self.enemy = Enemy("out.gif")
+        sg.theme('Black')
+        self.col1 = [[sg.Image(key = "-IMAGE-")], [sg.Button('  ', disabled = True, key ="-nextenemy-")]]
+        self.col2 = [
+            [sg.Text("Health: " + str(self.enemy.health), key = '-enemyhealth-')],
+            self.healthBar("EN",self.enemy.health),#0
+            [sg.Text("Enemy Action", key = '-enemyevent-')],
+            [sg.Text("Welcome to the game!", key = '-outcome-')],
+            [sg.Text("Your Action", key = '-playerevent-')],
+            [sg.Text('Score: ' + str(self.player.score), key = '-score-')],
+            [sg.Text("Health: " + str(self.player.health), key = '-playerhealth-')],
+            self.healthBar("US",self.player.health),#5
+            [sg.Button("Reload", key = '-action-')],
+            [sg.Button('Block', key = '-block-')]
+        ]
+        self.layout = [[sg.Column(self.col1)], [sg.Column(self.col2)]]
+        self.window = sg.Window('Shot Art Game', self.layout, finalize = True)
+        self.loop()
+
+    def healthBar(self, character, health):
+        data = []
+        for i in range(4):
+            if i < health:
+                data.append(sg.Image(character+"_FULL.png", key = "-" + character + "hbar" + str(i) + "-"))
+        return data
+
+    def loop(self):
+        event, values = self.window.read(timeout = 100)
+        self.window.Element('-IMAGE-').update_animation_no_buffering(self.enemy.path)
+        if event == sg.WIN_CLOSED:
+            self.window.close()
+        elif event == '-nextenemy-':
+            self.reset()
+        else:
+            if self.player.armed == None:
+                self.player.armed = False
+                self.enemy.armed = False
+                self.enemy.block = False
+            self.contest(event)
+            self.loop()
+
+    def playerAction(self, event):
+        if event == '-action-':
+            self.player.block = False
+            if self.player.armed == False:
+                self.window['-action-'].Update("Attack")
+                self.window['-playerevent-'].Update("You reloaded!")
+                self.player.armed = True
+                self.enemyAction()
+            elif self.player.armed == True:
+                self.window['-action-'].Update("Reload")
+                self.window['-playerevent-'].Update("You attacked!")
+                self.player.armed = False
+                self.enemyAction()
+        elif event == '-block-':
+            self.player.block = True
+            self.window['-playerevent-'].Update("You blocked!")
+            self.enemyAction()
+
+    def enemyAction(self):
+        action = random.randrange(1,self.enemy.block_tedency+1)
+        if action == self.enemy.block_tedency:
+            self.window['-enemyevent-'].Update("Enemy blocked!")
+            self.enemy.block = True
+        else:
+            self.enemy.block = False
+            if self.enemy.armed == False:
+                self.window['-enemyevent-'].Update("Enemy reloaded!")
+                self.enemy.armed = True
+            else:
+                self.window['-enemyevent-'].Update("Enemy attacked!")
+                self.enemy.armed = False
+
+    def playerHit(self):
+        if self.player.health - 1 == 0:
+            self.player.health -= 1
+            self.player.score -= 50
+            self.window['-playerhealth-'].Update("Health: " + str(self.player.health))
+            self.window['-UShbar' + str(self.player.health) +'-'].Update("US_EMPTY.png")
+            self.window['-outcome-'].Update("You lose! Game Over -50\nFinal Score:" + str(self.player.score))
+            self.window['-score-'].Update("Score: " + str(self.player.score))
+            self.window['-nextenemy-'].Update("Play again?")
+            self.window['-nextenemy-'].Update(disabled = False)
+            self.window['-action-'].Update(disabled = True)
+            self.window['-block-'].Update(disabled = True)
+            self.player.block = None
+            self.enemy.block = None
+        else:
+            self.player.health -= 1
+            self.player.score -= 10
+            self.window['-score-'].Update("Score: " + str(self.player.score))
+            self.window['-playerhealth-'].Update("Health: " + str(self.player.health))
+            self.window['-UShbar' + str(self.player.health) +'-'].Update("US_EMPTY.png")
+            self.playerHbar = self.healthBar("US",self.player.health)
+            self.player.block = None
+            self.enemy.block = None
+
+    def enemyHit(self):
+        if self.enemy.health - 1 == 0:
+            self.enemy.health -= 1
+            self.player.score += 20
+            self.window['-enemyhealth-'].Update("Health: " + str(self.enemy.health))
+            self.window['-ENhbar' + str(self.enemy.health) +'-'].Update("EN_EMPTY.png")
+            self.window['-outcome-'].Update("You won! +20\nCurrent score: " + str(self.player.score))
+            self.window['-nextenemy-'].Update(disabled = False)
+            self.window['-nextenemy-'].Update("Next enemy?")
+            self.window['-action-'].Update(disabled = True)
+            self.window['-block-'].Update(disabled = True)
+            self.window['-score-'].Update("Score: " + str(self.player.score))
+            self.player.block = None
+            self.enemy.block = None
+        else:
+            self.enemy.health -= 1
+            self.player.score += 10
+            self.window['-score-'].Update("Score: " + str(self.player.score))
+            self.window['-enemyhealth-'].Update("Health: " + str(self.enemy.health))
+            self.window['-ENhbar' + str(self.enemy.health) +'-'].Update("EN_EMPTY.png")
+            self.player.block = None
+            self.enemy.block = None
+
+    def bothHit(self):
+        if self.player.health - 1 == 0:
+            self.enemy.health -= 1
+            self.window['-enemyhealth-'].Update("Health: " + str(self.enemy.health))
+            self.window['-ENhbar' + str(self.enemy.health) +'-'].Update("EN_EMPTY.png")
+            self.playerHit()
+        elif self.enemy.health - 1 == 0:
+            self.player.health -= 1
+            self.window['-playerhealth-'].Update("Health: " + str(self.player.health))
+            self.window['-UShbar' + str(self.player.health) +'-'].Update("US_EMPTY.png")
+            self.enemyHit()
+        else:
+            self.enemyHit()
+            self.playerHit()
+
+
+    def contest(self, event):
+        self.playerAction(event)
+        if self.enemy.block == True and self.player.block == True: #both players block
+            self.window['-outcome-'].Update("Both of you blocked!")
+        elif self.enemy.block == True and self.player.block == False: #enemy blocks, player does not
+            if self.player.armed == False: #player can't shoot
+                self.window['-outcome-'].Update("Enemy blocked the attack!")#must reload if not
+            else:#player can shoot
+                self.window['-outcome-'].Update("Nothing happened!")#enemy is blocking
+        elif self.enemy.block == False and self.player.block == True: #enemy does not block, player does
+            if self.enemy.armed == False: #enemy can't shoot
+                self.window['-outcome-'].Update("You blocked the attack!") #enemy reloads
+            else: #enemy can shoot
+                self.window['-outcome-'].Update("Nothing happened!") #player is blocking
+        elif self.enemy.block == False and self.player.block == False: #neither block
+            if self.enemy.armed == False and self.player.armed == True: #enemy is unarmed, player is armed
+                self.window['-outcome-'].Update("You are hit!") #enemy is hit
+                self.playerHit()
+            elif self.enemy.armed == True and self.player.armed == False:
+                self.window['-outcome-'].Update("Enemy is hit!")
+                self.enemyHit()
+            elif self.enemy.armed == True and self.player.armed == True:
+                self.window['-outcome-'].Update("Both of you reloaded!")
+            elif self.enemy.armed == False and self.player.armed == False: #both are reloading
+                self.window['-outcome-'].Update("Both of you are hit!")
+                self.bothHit()
+            else:
+                self.window['-outcome-'].Update("")
+
+
+
+
+g = GameScreen()
---
+++ GenerateArt.py
@@ -0,0 +1,125 @@
+# Import all necessary libraries
+from PIL import Image, ImageDraw, ImageChops
+import random
+import colorsys
+import os
+
+# Define a function to generate a random colour
+def random_color():
+    h = random.random()
+    s = 1
+    v = 1
+
+    float_rgb = colorsys.hsv_to_rgb(h, s, v)
+    rgb = [int(x * 255) for x in float_rgb]
+
+    return tuple(rgb)
+
+# Define "interpolate" to be a function that blends all colours seamlessly in the final image
+def interpolate(start_color, end_color, factor: float):
+    recip = 1 - factor
+    return(
+        int(start_color[0] * recip + end_color[0] * factor),
+        int(start_color[1] * recip + end_color[1] * factor),
+        int(start_color[2] * recip + end_color[2] * factor)
+        )
+
+# Make image background transparent
+def convertImage(path):
+    img = Image.open(path)
+    img = img.convert("RGBA")
+    datas = img.getdata()
+    newData = []
+
+    for item in datas:
+        if item[0] == 0 and item[1] == 0 and item[2] == 0:
+            newData.append((0, 0, 0, 0))
+        else:
+            newData.append(item)
+
+    img.putdata(newData)
+    img.save(path, "PNG")
+
+# Define the main function
+def generate_art(path, target_size_px, scale_factor, lines):
+    print("Generating Art!")  # This line of code clarifies that the function is active / has been executed properly
+    image_size_px = target_size_px * scale_factor # Set image size
+    padding_px = 16 * scale_factor # Pad the image
+    image_bg_color = (0, 0, 0) # Set the background to black
+    start_color = random_color()
+    end_color = random_color()
+    image = Image.new(
+        "RGB", size=(image_size_px, image_size_px),
+                color=image_bg_color)
+
+    draw = ImageDraw.Draw(image)
+    points = [] # Set "points" to be an empty list to be used later in the code
+
+# The following lines of code determine the main constraints / conditions for the final image
+    for line in range(lines):
+        line2 = line
+        random_point = (
+            line + random.randint(padding_px, image_size_px - padding_px),
+            line2 - random.randint(padding_px, image_size_px - padding_px))
+        points.append(random_point)
+
+    min_x = min([p[0] for p in points])
+    max_x = max([p[0] for p in points])
+    min_y = min([p[1] for p in points])
+    max_y = max([p[1] for p in points])
+
+    delta_x = min_x - (image_size_px - max_x)
+    delta_y = min_y - (image_size_px - max_y)
+
+    for i, point in enumerate(points):
+        points[i] = (point[0] - delta_x // 2, point[1] - delta_y // 2)
+
+    thickness = 0
+    n_points = len(points) - 1
+
+    for i, point in enumerate(points):
+        overlay_image = Image.new(
+            "RGB", size=(image_size_px, image_size_px),
+            color=image_bg_color)
+        overlay_draw = ImageDraw.Draw((overlay_image))
+
+        p1 = point
+
+        if i == n_points:
+            p2 = points[0]
+        else:
+            p2 = points [i + 1]
+        line_xy = (p1, p2)
+        color_factor = i / n_points
+        line_color = interpolate(start_color, end_color, color_factor)
+        thickness += scale_factor
+        overlay_draw.line(line_xy, fill=line_color, width=thickness)
+        image = ImageChops.add(image, overlay_image)
+
+    image = image.resize((target_size_px, target_size_px))
+    img = image.save(path, "PNG")
+    img = convertImage(path)
+
+# Turns set of images into a GIF
+def createGIF(images):
+    open_images = []
+    for i in range(1,len(images)):
+        img = Image.open(images[i]+".png")
+        open_images.append(img)
+
+    img = Image.open(images[0]+".png")
+    img.save("out.gif", save_all=True, append_images=open_images, disposal=2, loop=0, duration=200)
+
+    for i in images:
+        os.remove(i+".png")
+
+# This line of code generates ten randomly generated images
+def start_generator(num):
+    images = []
+    for i in range(num):
+        path = "Test_Image_"+str(i)
+        lines = (i + 2) * 5
+        generate_art(path+".png", 120, 2, lines)
+        images.append(path)
+
+    createGIF(images)
