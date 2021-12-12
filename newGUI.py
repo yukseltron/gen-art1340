ca3da142db4aa030337f790ec673baf546d013c0
yukseltron <h.yuksel04@gmail.com>
2021-12-12 14:48:21 -0500

    modifying score updates

--- newGUI.py
+++ newGUI.py
@@ -7,22 +7,21 @@ class Enemy():
     def __init__(self, path):
         self.health = 4
         self.path = path
-        self.armed = False
+        self.armed = None
         self.block_tedency = 4
-        self.block = False;
+        self.block = None;
 
 class Player():
     def __init__(self):
         self.health = 4
-        self.armed = False
-        self.block = False;
+        self.armed = None
+        self.block = None
         self.score = 0
 
 class GameScreen():
     def __init__(self):
         self.player = Player()
         self.enemy = Enemy("out.gif")
-        self.round_score = 0
         sg.theme('Black')
         self.col1 = [[sg.Image(key = "-IMAGE-")], [sg.Button('  ', disabled = True, key ="-nextenemy-")]]
         self.col2 = [
@@ -30,7 +29,7 @@ class GameScreen():
             [sg.Text("Enemy Action", key = '-enemyevent-')],
             [sg.Text("Welcome to the game!", key = '-outcome-')],
             [sg.Text("Your Action", key = '-playerevent-')],
-            [sg.Text('Score: ' + str(self.player.score))],
+            [sg.Text('Score: ' + str(self.player.score), key = '-score-')],
             self.healthBar("US",self.player.health),#5
             [sg.Button("Reload", key = '-action-')],
             [sg.Button('Block', key = '-block-')]
@@ -54,6 +53,10 @@ class GameScreen():
         if event == sg.WIN_CLOSED:
             self.window.close()
         else:
+            if self.player.armed == None:
+                self.player.armed = False
+                self.enemy.armed = False
+                self.enemy.block = False
             self.contest(event)
             self.loop()
 
@@ -89,34 +92,71 @@ class GameScreen():
                 self.window['-enemyevent-'].Update("Enemy attacked!")
                 self.enemy.armed = False
 
+    def getHit(self):
+        if (self.player.health - 1) == 0:
+            self.player.score -= 50
+            self.window['-outcome-'].Update("You lose! Game Over -50\nFinal Score:" + str(self.player.score))
+            self.window['-score-'].Update("Score: " + str(self.player.score))
+            self.window['-nextenemy-'].Update("Play again?")
+            self.window['-action-'].Update(disabled = True)
+            self.window['-block-'].Update(disabled = True)
+            self.player.armed = None
+            self.player.block = None
+            self.enemy.armed = None
+            self.enemy.block = None
+        else:
+            self.player.health -= 1
+            self.player.score -= 10
+            self.window['-score-'].Update("Score: " + str(self.player.score))
+            self.col2[5] = self.healthBar("US",self.player.health)
+
+    def hit(self):
+        if (self.enemy.health - 1) == 0:
+            self.player.score += 20
+            self.window['-outcome-'].Update("You won! +20\nCurrent score: " + str(self.player.score))
+            self.window['-nextenemy-'].Update(disabled = False)
+            self.window['-nextenemy-'].Update("Next enemy?")
+            self.window['-action-'].Update(disabled = True)
+            self.window['-block-'].Update(disabled = True)
+            self.window['-score-'].Update("Score: " + str(self.player.score))
+            self.player.armed = None
+            self.player.block = None
+            self.enemy.armed = None
+            self.enemy.block = None
+        else:
+            self.enemy.health -= 1
+            self.player.score += 10
+            self.window['-score-'].Update("Score: " + str(self.player.score))
+            self.col2[0] = self.healthBar("EN",self.enemy.health)
+
+
     def contest(self, event):
         self.playerAction(event)
         if self.enemy.block == True and self.player.block == True: #both players block
-            self.window['-outcome-'].Update("Both of you blocked")
+            self.window['-outcome-'].Update("Both of you blocked!")
         elif self.enemy.block == True and self.player.block == False: #enemy blocks, player does not
             if self.player.armed == False: #player can't shoot
                 self.window['-outcome-'].Update("Enemy blocked the attack!")#must reload if not
             else:#player can shoot
-                self.window['-outcome-'].Update("")#enemy is blocking
+                self.window['-outcome-'].Update("Nothing happened!")#enemy is blocking
         elif self.enemy.block == False and self.player.block == True: #enemy does not block, player does
             if self.enemy.armed == False: #enemy can't shoot
                 self.window['-outcome-'].Update("You blocked the attack!") #enemy reloads
             else: #enemy can shoot
-                self.window['-outcome-'].Update("") #player is blocking
+                self.window['-outcome-'].Update("Nothing happened!") #player is blocking
         elif self.enemy.block == False and self.player.block == False: #neither block
             if self.enemy.armed == False and self.player.armed == True: #enemy is unarmed, player is armed
                 self.window['-outcome-'].Update("You are hit!") #enemy is hit
-                self.col2[0] = self.healthBar("EN",self.enemy.health-1)
-                if self.enemy.health == 0: #enemy dies
-                    self.window['-outcome-'].Update("You Won!")
-                    self.window['-nextenemy-'].Update("Next Enemy")
+                self.getHit()
             elif self.enemy.armed == True and self.player.armed == False:
                 self.window['-outcome-'].Update("Enemy is hit!")
-                self.col2[5] = self.healthBar("US",self.player.health-1)
+                self.hit()
             elif self.enemy.armed == True and self.player.armed == True:
-                self.window['-outcome-'].Update("")
-            else: #both are reloading
+                self.window['-outcome-'].Update("Both of you reloaded!")
+            elif self.enemy.armed == False and self.player.armed == False: #both are reloading
                 self.window['-outcome-'].Update("Both of you are hit!")
+            else:
+                self.window['-outcome-'].Update("")
 
 
 
