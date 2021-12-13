842cff03c3fd6198b50aa24dc171e3dd8af91069
yukseltron <h.yuksel04@gmail.com>
2021-12-13 13:38:52 -0500

    added text

--- GenerateText.py
+++ GenerateText.py
@@ -1,9 +1,9 @@
 import random
 
-nouns = ("darkness", "light", "passion", "time", "power", "strength", "vengeance", "tragedy", "mercy", "doom", "space", "evil")
-verbs = ("runs", "falls", "divides", "unleashes", "destroys", "manifests", "chooses", "rises", "grows", "breathes", "watches", "rages", "oozes", "laughs")
+nouns = ("darkness", "light", "passion", "time", "power", "strength", "vengeance", "tragedy", "mercy", "doom", "space", "evil", "good", "sky", "space", "stars", "galaxy", "universe", "domain")
+verbs = ("runs", "falls", "divides", "unleashes", "destroys", "manifests", "chooses", "rises", "grows", "breathes", "watches", "rages", "oozes", "laughs", "wrecks", "salvages", "climactic")
 adv = ("mightily.", "dutifully.", "foolishly.", "with strength.", "with rage.", "with pride.", "absolutely.", "with an unsteady rhythm.", "curiously.", "with unblinking eyes.", "aimlessly.")
 adj = ("Strong", "Weak", "Zealous", "Brave", "Fearsome", "Malevolent", "Tyrannical", "Furious", "Angry", "Wicked", "Judgemental", "Ancient", "Shimmering", "Vengeful", "Sleeping", "Chaotic", "Grim")
 
 def getRandText():
-    return '"' + adj[random.randrange(0,5)] + ' ' + nouns[random.randrange(0,5)] + ' ' + verbs[random.randrange(0,5)] + ' ' + adv[random.randrange(0,5)] + '"'
+    return '"' + adj[random.randrange(0,len(adj))] + ' ' + nouns[random.randrange(0,len(nouns))] + ' ' + verbs[random.randrange(0,len(verbs))] + ' ' + adv[random.randrange(0,len(adv))] + '"'
